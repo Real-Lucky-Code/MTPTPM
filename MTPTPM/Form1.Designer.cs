895a4ad0d4e47439cfb3c847184d1a862e2553a2
Real-Lucky-Code <tbaothiet@gmail.com>
2025-09-22 10:06:25 +0700

    Đổi lable

--- MTPTPM/Form1.Designer.cs
+++ MTPTPM/Form1.Designer.cs
@@ -40,9 +40,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(56, 65);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.Size = new System.Drawing.Size(98, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "UserName";
+            this.label1.Text = "Tên đăng nhập";
             // 
             // txtusername
             // 
@@ -58,7 +58,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(61, 16);
             this.label2.TabIndex = 2;
-            this.label2.Text = "mật khẩu";
+            this.label2.Text = "Mật khẩu";
             // 
             // txtpassword
             // 
